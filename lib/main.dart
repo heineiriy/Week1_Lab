dd182999ebbf481e8a9dc5ae4a250376bdccc1fe
Thea, Henry <423000602@nt.edu.ph>
2026-02-18 03:06:51 +0800

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lab 2',
+      title: 'flutterr demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
